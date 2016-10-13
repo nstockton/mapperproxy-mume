0bdcfbc2eee39295e2b513050b74c35f6b801a9e
Kat <blackmagicgirl@users.noreply.github.com>
2016-10-13 01:18:10 -0700

    Added MPL2 licence header to window.py. Removed unused grPointSize class and draw_dot method of Window class. Modified argument order and type for most of the draw_shape functions. Changed a few argument names. Made some that I won't use often keyword arguments with reasonable values. Removed extraneous blank lines. Changed variable names to be more consistent. Made draw_shape functions return the vertex lists from batch.add. This will make cleanup easier. Added ability to draw a border around a room. This is done by default for current room. Changed vec2d.py to use tab indents instead of spaces, to be project consistant. Fixed typos.

--- mapper/window.py
+++ mapper/window.py
@@ -1,3 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+###Some code borrowed from pymunk's debug drawing functions###
 import pyglet
 from pyglet.window import key
 from speechlight import Speech
@@ -24,18 +29,6 @@ class Color(namedtuple("Color", ["r","g","b","a"])):
 		return self[0]/255., self[1]/255., self[2]/255., self[3]/255.
 
 
-class grPointSize(pyglet.graphics.Group):
-	"""
-	This pyglet rendering group sets a specific point size.
-	"""
-	def __init__(self, size=1.0):
-		super(grPointSize, self).__init__()
-		self.size = size
-	def set_state(self):
-		pyglet.gl.glPointSize(self.size)
-	def unset_state(self):
-		pyglet.gl.glPointSize(1.0)
-
 class Window(pyglet.window.Window):
 	def __init__(self, world):
 		caption='MPM'
@@ -48,7 +41,6 @@ class Window(pyglet.window.Window):
 		self._gui_queue_lock = world._gui_queue_lock
 		self.batch=pyglet.graphics.Batch()
 		pyglet.clock.schedule_interval(self.queue_observer, 0.005)
-		self.vl=None
 		self.size=100.0
 
 	def queue_observer(self, dt):
@@ -87,26 +79,21 @@ class Window(pyglet.window.Window):
 	def do_toggle_fullscreen(self, sym, mod):
 		self.set_fullscreen(not self.fullscreen)
 
-	def draw_circle(self, pos, angle, radius, outline_color, fill_color):
-		circle_center = Vec2d(pos)
-		
+	def draw_circle(self, cp, radius, color, line_color=None, angle=0.0):
+		cp = Vec2d(cp)
 		#http://slabode.exofire.net/circle_draw.shtml
 		num_segments = int(4 * math.sqrt(radius))
 		theta = 2 * math.pi / num_segments
 		c = math.cos(theta)
 		s = math.sin(theta)
-		
 		x = radius # we start at angle 0
 		y = 0
-		
 		ps = []
-		
 		for i in range(num_segments):
-			ps += [Vec2d(circle_center.x + x, circle_center.y + y)]
+			ps += [Vec2d(cp.x + x, cp.y + y)]
 			t = x
 			x = c * x - s * y
 			y = s * t + c * y
-				
 		mode = pyglet.gl.GL_TRIANGLE_STRIP
 		ps2 = [ps[0]]
 		for i in range(1, int(len(ps)+1/2)):
@@ -116,34 +103,29 @@ class Window(pyglet.window.Window):
 		vs = []
 		for p in [ps[0]] + ps + [ps[-1]]:
 				vs += [p.x, p.y]
-			
-		c = circle_center + Vec2d(radius, 0).rotated(angle)
-		cvs = [circle_center.x, circle_center.y, c.x, c.y]
-			
+		c = cp + Vec2d(radius, 0).rotated(angle)
+		cvs = [cp.x, cp.y, c.x, c.y]
 		bg = pyglet.graphics.OrderedGroup(0)
 		fg = pyglet.graphics.OrderedGroup(1)
-			
 		l = len(vs)//2
-	   
-
-		self.batch.add(len(vs)//2, mode, bg,
+		if line_color is None:
+			line_color=color
+		return (self.batch.add(len(vs)//2, mode, bg,
 				('v2f', vs),
-				('c4B', fill_color.as_int()*l))
-		return self.batch.add(2, pyglet.gl.GL_LINES, fg,
+				('c4B', color.as_int()*l)), \
+		self.batch.add(2, pyglet.gl.GL_LINES, fg,
 				('v2f', cvs),
-				('c4B', outline_color.as_int()*2))
+				('c4B', line_color.as_int()*2)))
 
-
-	def draw_segment(self, a, b, color):
+	def draw_segment(self, a, b, color, group=None):
 		pv1 = Vec2d(a)
 		pv2 = Vec2d(b)
 		line = (int(pv1.x), int(pv1.y), int(pv2.x), int(pv2.y))
-		
-		return self.batch.add(2, pyglet.gl.GL_LINES, None,
+		return self.batch.add(2, pyglet.gl.GL_LINES, group,
 					('v2i', line),
 					('c4B', color.as_int() * 2))
 
-	def draw_fat_segment(self, a, b, radius, outline_color, fill_color):
+	def draw_fat_segment(self, a, b, radius, color, group=None):
 		pv1 = Vec2d(a)
 		pv2 = Vec2d(b)
 		d = pv2 - pv1
@@ -157,11 +139,11 @@ class Window(pyglet.window.Window):
 		p4 = pv2 - Vec2d(dx,dy)
 		vs = [i for xy in [p1,p2,p3]+[p2,p3,p4] for i in xy]
 		l = len(vs)//2
-		return self.batch.add(l,pyglet.gl.GL_TRIANGLES, None,
+		return self.batch.add(l,pyglet.gl.GL_TRIANGLES, group,
 					('v2f', vs),
-					('c4B', fill_color.as_int() * l))
+					('c4B', color.as_int() * l))
 
-	def draw_polygon(self, verts, color):
+	def draw_polygon(self, verts, color, group=None):
 		ps = verts
 		mode = pyglet.gl.GL_TRIANGLE_STRIP
 		ps = [ps[1],ps[2], ps[0]] + ps[3:]
@@ -169,15 +151,10 @@ class Window(pyglet.window.Window):
 		for p in [ps[0]] + ps + [ps[-1]]:
 				vs += [p.x, p.y]
 		l = len(vs)//2
-		return self.batch.add(l, mode, None,
+		vl=self.batch.add(l, mode, group,
 					('v2f', vs),
 					('c4B', color.as_int()*l))
-
-	def draw_dot(self, size, pos, color):
-		# todo: optimize this functions
-		return self.batch.add(1, pyglet.gl.GL_POINTS, grPointSize(size), 
-					('v2f', pos),
-					('c4B', color.as_int()*1))
+		return vl
 
 	@property
 	def cx(self):
@@ -189,13 +166,21 @@ class Window(pyglet.window.Window):
 	def cp(self):
 		return Vec2d(self.cx,self.cy)
 
-
-	def draw_room(self, room, cp):
+	def draw_room(self, room, cp, outline_color=None):
 		color=Color(*TERRAIN_COLORS[room.terrain])
 		d=self.size/2.0
 		vs=[cp-d, cp-(d,d*-1), cp+d, cp+(d,d*-1)]
-		self.draw_polygon(vs,color)
-
+		vl=self.draw_polygon(vs,color, group=pyglet.graphics.OrderedGroup(2))
+		if outline_color is not None:
+			vl=[vl]
+			#vl=[vl, self.draw_circle(cp, self.size*.85, outline_color)]
+			d *= 1.1
+			vs=[cp-d, cp-(d,d*-1), cp+d, cp+(d,d*-1)]
+			vl.append(self.draw_polygon(vs,Color(0,0,0,255), group=pyglet.graphics.OrderedGroup(1)))
+			d *= 1.6
+			vs=[cp-d, cp-(d,d*-1), cp+d, cp+(d,d*-1)]
+			vl.append(self.draw_polygon(vs,outline_color, group=pyglet.graphics.OrderedGroup(0)))
+		return vl
 
 	def num_rooms_to_draw(self):
 		rooms_w=(self.width//self.size)//2
@@ -203,11 +188,11 @@ class Window(pyglet.window.Window):
 		return (int(rooms_w),int(rooms_h),1)
 
 	def draw_rooms(self, currentRoom):
-		self.draw_room(currentRoom, self.cp)
+		self.draw_room(currentRoom, self.cp, outline_color=Color(255,222,0,255))
 		for r in self.world.getVisibleNeighbors(roomObj=currentRoom, radius=self.num_rooms_to_draw()):
 			if r[4] is 0:
 				room=r[1]
-				d=Vec2d(r[2],r[3])*(self.size*1.2)
+				d=Vec2d(r[2],r[3])*(self.size*2)
 				self.draw_room(room,self.cp+d)
 
 Window.register_event_type('on_map_sync')
