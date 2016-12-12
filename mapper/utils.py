10c4e376b0722152ecddb24acf098fe7429e0519
Nick Stockton <nstockton@users.noreply.github.com>
2016-12-11 23:34:30 -0800

    Update the paging function to first join and split lines before processing them. This is necessary in case one of the strings in the lines list has multiple lines in it.

--- mapper/utils.py
+++ mapper/utils.py
@@ -68,6 +68,7 @@ def decodeBytes(data):
 
 def page(lines):
 	"""Output word wrapped lines using the 'more' shell command if necessary."""
+	lines = "\n".join(lines).splitlines()
 	width, height = terminalsize.get_terminal_size()
 	# Word wrapping to 1 less than the terminal width is necessary to prevent occasional blank lines in the terminal output.
 	text = "\n".join(textwrap.fill(line.strip(), width - 1) for line in lines)
