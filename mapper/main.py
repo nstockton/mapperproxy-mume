1a2cefd8d1100709f90cdb203b307949d73cd072
Nick Stockton <nstockton@users.noreply.github.com>
2016-12-11 20:24:41 -0800

    Import the print_function from __future__ for all files that use print.

--- mapper/main.py
+++ mapper/main.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from __future__ import print_function
+
 import socket
 from telnetlib import IAC, DONT, DO, WONT, WILL, theNULL, SB, SE, GA, TTYPE, NAWS
 import threading
