nstockton/mapperproxy-mume

commit 10ec2b3d777ff4cb19d5a56700ad96ef91aa432f

Set up the option to choose the GUI at startup.

data/config.json.sample
@@ -119,5 +119,5 @@
       ]
     }
   },
-  "use_gui": true
+  "use_gui": "default"
 }

mapper/__init__.py
@@ -30,7 +30,7 @@ with config_lock:
 	try:
 		use_gui = c['use_gui']
 	except KeyError:
-		c['use_gui'] = use_gui = True
+		c['use_gui'] = use_gui = "default"
 		c.save()
 	del c
 

mapper/emulation.py
@@ -242,7 +242,7 @@ class Emulator(threading.Thread):
 		# The user has typed 'q[uit]'. Save the config file and exit.
 		wld.saveConfig()
 		wld.output("Good bye.")
-		if not self._use_gui:
+		if self._use_gui is None:
 			return
 		with wld._gui_queue_lock:
 			wld._gui_queue.put(None)
@@ -251,14 +251,14 @@ class Emulator(threading.Thread):
 def main(use_gui=None):
 	if use_gui is None:
 		from . import use_gui
-	if use_gui:
+	if use_gui is not None:
 		try:
 			import pyglet
 		except ImportError:
 			print("Unable to find pyglet. Disabling gui")
-			use_gui = False
+			use_gui = None
 	emulator_thread=Emulator(use_gui)
 	emulator_thread.start()
-	if use_gui:
+	if use_gui is not None:
 		pyglet.app.run()
 	emulator_thread.join()

mapper/main.py
@@ -281,7 +281,7 @@ class Server(threading.Thread):
 				self.close()
 				continue
 			del clientBuffer[:]
-		if self._use_gui:
+		if self._use_gui is not None:
 			# Shutdown the gui
 			with self._mapper._gui_queue_lock:
 				self._mapper._gui_queue.put(None)
@@ -294,12 +294,12 @@ def main(outputFormat="normal", use_gui=None):
 	outputFormat = outputFormat.strip().lower()
 	if use_gui is None:
 		from . import use_gui
-	if use_gui:
+	if use_gui is not None:
 		try:
 			import pyglet
 		except ImportError:
 			print("Unable to find pyglet. Disabling the GUI")
-			use_gui = False
+			use_gui = None
 	proxySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	proxySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	proxySocket.bind(("", 4000))
@@ -326,7 +326,7 @@ def main(outputFormat="normal", use_gui=None):
 	serverThread.start()
 	proxyThread.start()
 	mapperThread.start()
-	if use_gui:
+	if use_gui is not None:
 		pyglet.app.run()
 	serverThread.join()
 	try:

mapper/world.py
@@ -72,15 +72,18 @@ class Exit(object):
 
 
 class World(object):
-	def __init__(self, use_gui=True):
+	def __init__(self, use_gui=None):
 		self.isSynced = False
 		self.rooms = {}
 		self.labels = {}
 		self._use_gui = use_gui
-		if use_gui:
+		if use_gui is not None:
 			self._gui_queue = Queue()
 			self._gui_queue_lock = threading.Lock()
-			from .window import Window
+			if use_gui == "sighted":
+				from .sighted import Window
+			else:
+				from .window import Window
 			self.window=Window(self)
 		self._currentRoom = None
 		self.loadRooms()
@@ -93,7 +96,7 @@ class World(object):
 	@currentRoom.setter
 	def currentRoom(self, value):
 		self._currentRoom = value
-		if self._use_gui:
+		if self._use_gui is not None:
 			with self._gui_queue_lock:
 				self._gui_queue.put(('on_map_sync', value))
 

start.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="The accessible Mume mapper.")
 	parser.add_argument("-e", "--emulation", help="Start in emulation mode.", action="store_true")
 	group = parser.add_mutually_exclusive_group()
-	group.add_argument("-g", "--gui", help="Use the GUI (requires pyglet).", action="store_true", default=None)
+        group.add_argument("-g", "--gui", help="Use a GUI (requires pyglet).", action="store", default="default")
 	group.add_argument("-t", "--text", help="Text-only mode (no GUI).", action="store_false", default=None)
 	parser.add_argument("-f", "--format", help="Select how data from the server is transformed before  being sent to the client.", choices=["normal", "tintin", "raw"], default="normal")
 	args = parser.parse_args()

commit d531f53d81d6b286b26df0941b8371f37c6c0bb7

Fix loading the mapper in text-only mode: was broken by 10ec2b3d777ff4cb19d5a56700ad96ef91aa432f. Rename mapper/window.py -> mapper/hc.py (short for high contrast). Restrict the possible user supplied arguments of the -g/--gui start flag to 'hc' and 'sighted'. If the -g/--gui start flag is not supplied and neither is -t/--text, load the mapper with GUI enabled, using the GUI type stored in the configuration file. Change occurrences of single quotes (') for string definitions in mapper/__init__.py to double quotes (") to match the style of the project. Also add spaces around operators where there were none, and rename the single letter 'c' instance of config to cfg for clarity.

data/config.json.sample
@@ -119,5 +119,5 @@
       ]
     }
   },
-  "use_gui": "default"
+  "use_gui": "hc"
 }

mapper/__init__.py
@@ -6,9 +6,9 @@ import logging
 from .config import Config, config_lock
 
 with config_lock:
-	c=Config()
+	cfg = Config()
 	try:
-		debug_level = c['debug_level']
+		debug_level = cfg["debug_level"]
 		if debug_level not in logging._levelNames:
 			if isinstance(debug_level, int):
 				if debug_level * 10 in logging._levelNames:
@@ -24,17 +24,18 @@ with config_lock:
 	except (KeyError, AttributeError):
 		debug_level = None
 	finally:
-		if 'debug_level' not in c or debug_level != c['debug_level']:
-			c['debug_level'] = debug_level
+		if "debug_level" not in cfg or debug_level != cfg["debug_level"]:
+			cfg["debug_level"] = debug_level
 			c.save()
 	try:
-		use_gui = c['use_gui']
+		use_gui = cfg["use_gui"]
 	except KeyError:
-		c['use_gui'] = use_gui = "default"
-		c.save()
-	del c
+		cfg["use_gui"] = use_gui = "hc"
+		cfg.save()
+	del cfg
 
 if debug_level is not None or debug_level != 0:
-	logging.basicConfig(filename='debug.log', filemode='w', level=debug_level, format='%(levelname)s: from %(name)s in %(threadName)s: "%(message)s" @ %(asctime)s.%(msecs)d', datefmt='%m/%d/%Y %H:%M:%S')
-	logging.info('Initializing')
-else: del logging
+	logging.basicConfig(filename="debug.log", filemode="w", level=debug_level, format="%(levelname)s: from %(name)s in %(threadName)s: \"%(message)s\" @ %(asctime)s.%(msecs)d", datefmt="%m/%d/%Y %H:%M:%S")
+	logging.info("Initializing")
+else:
+	del logging

mapper/emulation.py
@@ -242,23 +242,24 @@ class Emulator(threading.Thread):
 		# The user has typed 'q[uit]'. Save the config file and exit.
 		wld.saveConfig()
 		wld.output("Good bye.")
-		if self._use_gui is None:
-			return
-		with wld._gui_queue_lock:
-			wld._gui_queue.put(None)
+		if self._use_gui:
+			with wld._gui_queue_lock:
+				wld._gui_queue.put(None)
 
 
-def main(use_gui=None):
-	if use_gui is None:
+def main(use_gui=True):
+	if use_gui is True:
+		# The user wants to use a GUI, but didn't specify which one. Grab the preferred GUI option from the configuration.
 		from . import use_gui
-	if use_gui is not None:
+	if use_gui :
+		use_gui = use_gui.strip().lower()
 		try:
 			import pyglet
 		except ImportError:
 			print("Unable to find pyglet. Disabling gui")
-			use_gui = None
+			use_gui = False
 	emulator_thread=Emulator(use_gui)
 	emulator_thread.start()
-	if use_gui is not None:
+	if use_gui:
 		pyglet.app.run()
 	emulator_thread.join()

mapper/main.py
@@ -281,7 +281,7 @@ class Server(threading.Thread):
 				self.close()
 				continue
 			del clientBuffer[:]
-		if self._use_gui is not None:
+		if self._use_gui:
 			# Shutdown the gui
 			with self._mapper._gui_queue_lock:
 				self._mapper._gui_queue.put(None)
@@ -290,16 +290,18 @@ class Server(threading.Thread):
 			mpiThread.join()
 
 
-def main(outputFormat="normal", use_gui=None):
+def main(outputFormat="normal", use_gui=True):
 	outputFormat = outputFormat.strip().lower()
-	if use_gui is None:
+	if use_gui is True:
+		# The user wants to use a GUI, but didn't specify which one. Grab the preferred GUI option from the configuration.
 		from . import use_gui
-	if use_gui is not None:
+	if use_gui:
+		use_gui = use_gui.strip().lower()
 		try:
 			import pyglet
 		except ImportError:
 			print("Unable to find pyglet. Disabling the GUI")
-			use_gui = None
+			use_gui = False
 	proxySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	proxySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	proxySocket.bind(("", 4000))
@@ -326,7 +328,7 @@ def main(outputFormat="normal", use_gui=None):
 	serverThread.start()
 	proxyThread.start()
 	mapperThread.start()
-	if use_gui is not None:
+	if use_gui:
 		pyglet.app.run()
 	serverThread.join()
 	try:

mapper/world.py
@@ -77,13 +77,13 @@ class World(object):
 		self.rooms = {}
 		self.labels = {}
 		self._use_gui = use_gui
-		if use_gui is not None:
+		if use_gui:
 			self._gui_queue = Queue()
 			self._gui_queue_lock = threading.Lock()
-			if use_gui == "sighted":
+			if use_gui == "hc":
+				from .hc import Window
+			elif use_gui == "sighted":
 				from .sighted import Window
-			else:
-				from .window import Window
 			self.window=Window(self)
 		self._currentRoom = None
 		self.loadRooms()
@@ -96,7 +96,7 @@ class World(object):
 	@currentRoom.setter
 	def currentRoom(self, value):
 		self._currentRoom = value
-		if self._use_gui is not None:
+		if self._use_gui:
 			with self._gui_queue_lock:
 				self._gui_queue.put(('on_map_sync', value))
 

start.py
@@ -12,15 +12,15 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="The accessible Mume mapper.")
 	parser.add_argument("-e", "--emulation", help="Start in emulation mode.", action="store_true")
 	group = parser.add_mutually_exclusive_group()
-	group.add_argument("-g", "--gui", help="Use a GUI (requires pyglet).", action="store", default="default")
-	group.add_argument("-t", "--text", help="Text-only mode (no GUI).", action="store_false", default=None)
+	group.add_argument("-g", "--gui", help="Use a GUI (requires pyglet).", choices=["hc", "sighted"], default=True)
+	group.add_argument("-t", "--text", help="Text-only mode (disables the GUI).", action="store_true", default=False)
 	parser.add_argument("-f", "--format", help="Select how data from the server is transformed before  being sent to the client.", choices=["normal", "tintin", "raw"], default="normal")
 	args = parser.parse_args()
 	try:
 		if args.emulation:
-			mapper.emulation.main(use_gui=args.gui or args.text)
+			mapper.emulation.main(use_gui=False if args.text else args.gui)
 		else:
-			mapper.main.main(outputFormat=args.format.lower(), use_gui=args.gui or args.text)
+			mapper.main.main(outputFormat=args.format.lower(), use_gui=False if args.text else args.gui)
 	except:
 		traceback.print_exception(*sys.exc_info())
 		logging.exception("OOPS!")