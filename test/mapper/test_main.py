3b9f3019c7a6c2439cbaa86585fede1c857aa140
Ted <TheodoreCooke@trentu.ca>
2019-11-07 02:53:40 -0500

    test that both initial output and welcome message are passed to the client

--- test/mapper/test_main.py
+++ test/mapper/test_main.py
@@ -1,10 +1,12 @@
 import unittest
 from unittest.mock import call, Mock
 from telnetlib import ECHO, IAC, NAWS, SB, TTYPE, WILL
-from time import sleep
 from queue import Empty, Queue
 
 from mapper.main import *
+
+initialOutput = IAC+DO+TTYPE+IAC+DO+NAWS
+welcomeMessage = b"\r\n                              ***  MUME VIII  ***\r\n\r\n"
 
 
 class TestServerThread(unittest.TestCase):
@@ -22,9 +24,7 @@ class TestServerThread(unittest.TestCase):
 		serverSocket.recv.side_effect = lambda arg: outputFromMume.get()
 		serverSocket.sendall.side_effect = lambda data: inputToMume.put(data)
 		clientSocket = Mock(spec=socket.socket)
-		inputFromUser = Queue()
 		outputToUser = Queue()
-		clientSocket.recv.side_effect = lambda arg: inputFromUser.get()
 		clientSocket.sendall.side_effect = lambda data: outputToUser.put(data)
 
 		serverThread = Server(
@@ -35,10 +35,11 @@ class TestServerThread(unittest.TestCase):
 			interface="text",
 			promptTerminator=None
 		)
+		serverThread.daemon = True
 		serverThread.start()
 
 		# test that the initial configuration parameters are outputted when the server first replies
-		outputFromMume.put(IAC+DO+TTYPE+IAC+DO+NAWS,)
+		outputFromMume.put(initialOutput)
 		try:
 			while initialConfiguration:
 				data = inputToMume.get(timeout=1)
@@ -50,11 +51,22 @@ class TestServerThread(unittest.TestCase):
 		if not inputToMume.empty():
 			remainingOutput = inputToMume.get()
 			raise AssertionError("The server thread spat out at least one unexpected initial configuration: "+str(remainingOutput))
-		
-		#  out comes start screen
-		#assert it comes out other end
-		#in goes username
-		#assert comes out
+
+		# test initial telnet negociation was passed to the client
+		try:
+			data = outputToUser.get(timeout=1)
+			self.assertEqual(data, initialOutput)
+		except Empty:
+			raise AssertionError("initial telnet negociations were not passed to the client")
+			
+		# test regular text is passed through to the client
+		try:
+			outputFromMume.put(welcomeMessage)
+			clientWelcomeMessage = outputToUser.get(timeout=1)
+			self.assertEqual(welcomeMessage, clientWelcomeMessage)
+		except Empty:
+			raise AssertionError("The welcome message was not passed through to the client within 1 second.")
+
 		#in goes some more negociation
 		#assert it comes out
 		# put in password
@@ -63,8 +75,6 @@ class TestServerThread(unittest.TestCase):
 		 # assert thread is dead
 		
 		serverThreadInput = [  # list binary input in chronological order
-			b"\r\n                              ***  MUME VIII  ***\r\n\r\n                              In progress at FIRE\r\n                     (Free Internet Roleplay Experiences)\r\n                      Hosted at HEIG-VD (www.heig-vd.ch)\r\n\r\n             Adapted from J.R.R. Tolkien's Middle-earth world and\r\n                   maintained by CryHavoc, Manwe, and Nada.\r\n\r\n              Original code DikuMUD I (help credits), created by:\r\n       S. Hammer, T. Madsen, K. Nyboe, M. Seifert, and H.H. Staerfeldt.\r\n\r\nIf you have never played MUME before, type NEW to create a new character,\r\nor ? for help. Otherwise, type your account or character name.\r\n\r\n\r\n",
-			b"By what name do you wish to be known? ",
 			IAC+DO+b"*"+IAC+SB+TTYPE+ECHO+IAC+SE,
 			b"IACSB*\x02US-ASCIIIACSE",
 			b"",
