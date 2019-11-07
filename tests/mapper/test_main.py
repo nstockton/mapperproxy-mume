0a5958ef0a1bca552f3d0816d60c8980b1d5187a
Nick Stockton <nstockton@users.noreply.github.com>
2019-11-07 03:14:14 -0800

    Fix Flake8 warnings for tests/mapper/test_main.py.

--- tests/mapper/test_main.py
+++ tests/mapper/test_main.py
@@ -2,14 +2,17 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import unittest
-from unittest.mock import call, Mock
 
-from mapper.main import *
-from telnetlib import CHARSET, ECHO, IAC, NAWS, RCP, SB, TTYPE, WILL
+import socket
+import unittest
+from unittest.mock import Mock
+
+from mapper.main import Server
+from telnetlib import CHARSET, ECHO, IAC, DO, NAWS, RCP, SB, SE, TTYPE, WILL
 from queue import Empty, Queue
 
-INITIAL_OUTPUT = IAC+DO+TTYPE+IAC+DO+NAWS
+
+INITIAL_OUTPUT = IAC + DO + TTYPE + IAC + DO + NAWS
 WELCOME_MESSAGE = b"\r\n                              ***  MUME VIII  ***\r\n\r\n"
 
 
@@ -19,9 +22,8 @@ class TestServerThread(unittest.TestCase):
 			b"~$#EI\n",
 			b"~$#EX2\n3G\n",
 			b"~$#EP2\nG\n",
-			IAC+WILL+CHARSET,
+			IAC + WILL + CHARSET,
 		]
-
 		mumeSocket = Mock(spec=socket.socket)
 		outputFromMume = Queue()
 		inputToMume = Queue()
@@ -30,7 +32,6 @@ class TestServerThread(unittest.TestCase):
 		clientSocket = Mock(spec=socket.socket)
 		outputToUser = Queue()
 		clientSocket.sendall.side_effect = lambda data: outputToUser.put(data)
-
 		serverThread = Server(
 			client=clientSocket,
 			server=mumeSocket,
@@ -41,30 +42,32 @@ class TestServerThread(unittest.TestCase):
 		)
 		serverThread.daemon = True  # otherwise if this does not terminate, it prevents unittest from terminating
 		serverThread.start()
-
 		# test when the server sends its initial negociations, the server thread outputs its initial configuration
 		outputFromMume.put(INITIAL_OUTPUT)
 		try:
 			while initialConfiguration:
 				data = inputToMume.get(timeout=1)
-				self.assertIn(data, initialConfiguration, "Unknown initial configuration: "+str(data))
+				self.assertIn(data, initialConfiguration, "Unknown initial configuration: " + str(data))
 				initialConfiguration.remove(data)
 		except Empty:
-			raise AssertionError("The server thread did not output the expected number of configuration parameters.\
-				The yet-to-be-seen configurations are: "+"; ".join(initialConfiguration))
-
+			raise AssertionError(
+				"The server thread did not output the expected number of configuration parameters."
+				+ "The yet-to-be-seen configurations are: "
+				+ "; ".join(initialConfiguration)
+			)
 		# test nothing extr has been sent yet
 		if not inputToMume.empty():
 			remainingOutput = inputToMume.get()
-			raise AssertionError("The server thread spat out at least one unexpected initial configuration: "+str(remainingOutput))
-
+			raise AssertionError(
+				"The server thread spat out at least one unexpected initial configuration: "
+				+ str(remainingOutput)
+			)
 		# test initial telnet negociations were passed to the client
 		try:
 			data = outputToUser.get(timeout=1)
 			self.assertEqual(data, INITIAL_OUTPUT)
 		except Empty:
 			raise AssertionError("initial telnet negociations were not passed to the client")
-
 		# test regular text is passed through to the client
 		try:
 			outputFromMume.put(WELCOME_MESSAGE)
@@ -72,11 +75,10 @@ class TestServerThread(unittest.TestCase):
 			self.assertEqual(WELCOME_MESSAGE, data)
 		except Empty:
 			raise AssertionError("The welcome message was not passed through to the client within 1 second.")
-
 		# test further telnet negociations are passed to the client with the exception of charset negociations
 		try:
-			charsetNegociation = IAC+DO+CHARSET+IAC+SB+TTYPE+ECHO+IAC+SE
-			charsetSubnegociation = IAC+SB+CHARSET+RCP+b"US-ASCII"+IAC+SE
+			charsetNegociation = IAC + DO + CHARSET + IAC + SB + TTYPE + ECHO + IAC + SE
+			charsetSubnegociation = IAC + SB + CHARSET + RCP + b"US-ASCII" + IAC + SE
 			outputFromMume.put(charsetNegociation)
 			data = outputToUser.get(timeout=1)
 			self.assertEqual(data, charsetNegociation[3:])  # slicing off the charset negociation
@@ -85,7 +87,6 @@ class TestServerThread(unittest.TestCase):
 			self.assertEqual(data, b"")
 		except Empty:
 			raise AssertionError("Further telnet negociations were not passed to the client")
-
 		# when mume outputs further text, test it is passed to the user
 		try:
 			usernamePrompt = b"By what name do you wish to be known? "
@@ -94,7 +95,6 @@ class TestServerThread(unittest.TestCase):
 			self.assertEqual(data, usernamePrompt)
 		except Empty:
 			raise AssertionError("Further text was not passed to the user")
-
 		# when mume outputs an empty string, test server thread closes within a second
 		outputFromMume.put(b"")
 		serverThread.join(1)
