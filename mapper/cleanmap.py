5ef678932117a514ad05a43617d83a909cbc5749
Ted <TheodoreCooke@trentu.ca>
2019-11-27 06:02:49 -0500

    add class ExitsCleaner to remove hidden doors where the game clearly shows no such door exists
    Currently only tested for rooms with zero or one exit

---
+++ mapper/cleanmap.py
@@ -0,0 +1,26 @@
+﻿# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import re
+
+from .mudevents import Handler
+from .world import DIRECTIONS
+
+directionsRegexp = "|".join([d.title() for d in DIRECTIONS])
+exitRegexp = re.compile(
+	r".*(?<![#(])(?P<dir>" + directionsRegexp + r")(?![#)]).* +- .*"
+)
+
+
+class ExitsCleaner(Handler):
+	def handle(self, data):
+		if data.startswith("Exits:"):
+			return
+		for line in data.split("\r\n"):
+			m = exitRegexp.match(line)
+			if m:
+				room = self.mapper.currentRoom
+				dir = m.group("dir").lower()
+				if self.mapper.isSynced and dir in room.exits and "hidden" in room.exits[dir].doorFlags:
+					self.mapper.user_command_secret("remove " + dir)
