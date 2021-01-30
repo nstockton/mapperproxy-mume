fd909f861ef73b11d7086f475faf23a2ff9a9bb3
Nick Stockton <nstockton@users.noreply.github.com>
2021-01-30 07:35:14 -0800

    Add tests and docstrings for mapper/roomdata/database.py The room and label databases are now verified using JSon Schemas for each load and save.

--- mapper/roomdata/database.py
+++ mapper/roomdata/database.py
@@ -9,22 +9,23 @@ from __future__ import annotations
 # Built-in Modules:
 import codecs
 import json
+import logging
 import os.path
 from typing import Any, Dict, List, Mapping, Tuple, Union
+
+# Third-party Modules:
+import jsonschema
+import rapidjson
 
 # Local Modules:
 from ..utils import getDirectoryPath
 
 
-try:
-	import rapidjson
-except ImportError:
-	rapidjson = None
-
-
 DATA_DIRECTORY: str = getDirectoryPath("data")
 LABELS_FILE: str = "room_labels.json"
 SAMPLE_LABELS_FILE: str = "room_labels.json.sample"
+LABELS_SCHEMA_FILE: str = "room_labels.json.schema"
+LABELS_SCHEMA_FILE_PATH: str = os.path.join(DATA_DIRECTORY, LABELS_SCHEMA_FILE)
 LABELS_FILE_PATH: str = os.path.join(DATA_DIRECTORY, LABELS_FILE)
 SAMPLE_LABELS_FILE_PATH: str = os.path.join(DATA_DIRECTORY, SAMPLE_LABELS_FILE)
 MAP_FILE: str = "arda.json"
@@ -32,68 +33,148 @@ SAMPLE_MAP_FILE: str = "arda.json.sample"
 MAP_DIRECTORY: str = getDirectoryPath("maps")
 MAP_FILE_PATH: str = os.path.join(MAP_DIRECTORY, MAP_FILE)
 SAMPLE_MAP_FILE_PATH: str = os.path.join(MAP_DIRECTORY, SAMPLE_MAP_FILE)
+MAP_SCHEMA_FILE: str = "map.json.schema"
+MAP_SCHEMA_FILE_PATH: str = os.path.join(DATA_DIRECTORY, MAP_SCHEMA_FILE)
 
 
-def _load(filePath: str) -> Tuple[Union[str, None], Union[Dict[str, Any], None]]:
-	if os.path.exists(filePath):
-		if not os.path.isdir(filePath):
-			try:
-				with codecs.open(filePath, "rb", encoding="utf-8") as fileObj:
-					return None, json.load(fileObj)
-			except IOError as e:
-				return f"{e.strerror}: '{e.filename}'", None
-			except ValueError:
-				return f"Corrupted database file: {filePath}", None
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+class SchemaValidationError(ValueError):
+	"""Raised if there was an error validating an object's schema."""
+
+
+def _validate(database: Mapping[str, Any], schemaPath: str) -> None:
+	"""
+	Validates a database against a schema.
+
+	Note:
+		The `jsonschema` library validates python data structures directly and
+		produces nice error messages, but validation is slow.
+		The `rapidjson` library validates much faster, however it produces poor error messages.
+		For this reason rapidjson is used for the initial
+		validation, and jsonschema is used if there is a failure.
+
+	Args:
+		database: The database to be validated.
+		schemaPath: The location of the schema.
+	"""
+	with codecs.open(schemaPath, "rb", encoding="utf-8") as fileObj:
+		schema: Dict[str, Any] = json.load(fileObj)
+	validate: rapidjson.Validator = rapidjson.Validator(rapidjson.dumps(schema))
+	try:
+		validate(rapidjson.dumps(database))
+	except rapidjson.ValidationError as rapidjsonExc:
+		try:
+			jsonschema.validate(database, schema)
+		except jsonschema.ValidationError as jsonschemaExc:
+			raise SchemaValidationError(str(jsonschemaExc)) from jsonschemaExc
 		else:
-			return f"Error: '{filePath}' is a directory, not a file.", None
-	else:
-		return f"Error: '{filePath}' doesn't exist.", None
+			logger.warning(
+				f"Error: jsonschema did not raise an exception, whereas rapidjson raised {rapidjsonExc}."
+			)
+			raise SchemaValidationError(str(rapidjsonExc)) from rapidjsonExc
 
 
-def loadLabels() -> Tuple[Union[str, None], Union[Dict[str, str], None]]:
+def _load(databasePath: str, schemaPath: str) -> Union[Tuple[str, None], Tuple[None, Dict[str, Any]]]:
+	"""
+	Loads a database into memory.
+
+	Args:
+		databasePath: The location of the database.
+		schemaPath: The location of the schema.
+
+	Returns:
+		An error message or None, and the loaded database or None.
+	"""
+	if not os.path.exists(databasePath):
+		return f"Error: '{databasePath}' doesn't exist.", None
+	elif os.path.isdir(databasePath):
+		return f"Error: '{databasePath}' is a directory, not a file.", None
+	try:
+		with codecs.open(databasePath, "rb", encoding="utf-8") as fileObj:
+			database: Dict[str, Any] = json.load(fileObj)
+		_validate(database, schemaPath)
+		return None, database
+	except IOError as e:
+		return f"{e.strerror}: '{e.filename}'", None
+	except ValueError:
+		return f"Corrupted database file: {databasePath}", None
+
+
+def _dump(database: Mapping[str, Any], databasePath: str, schemaPath: str) -> None:
+	"""
+	Saves a database to disk.
+
+	Args:
+		database: The database to be saved.
+		databasePath: The location where the database should be saved.
+		schemaPath: The location of the schema.
+	"""
+	with codecs.open(databasePath, "wb", encoding="utf-8") as fileObj:
+		_validate(database, schemaPath)
+		rapidjson.dump(database, fileObj, sort_keys=True, indent=2, chunk_size=2 ** 16)
+
+
+def loadLabels() -> Union[Tuple[str, None], Tuple[None, Dict[str, str]]]:
+	"""
+	Loads the labels database into memory.
+
+	The default label definitions are first loaded, then the user's label definitions are merged in.
+
+	Returns:
+		An error message or None, and the loaded labels database or None.
+	"""
 	errorMessages: List[str] = []
 	labels: Dict[str, str] = {}
-	loaded: bool = False
-	# First load the sample labels.
-	errors, result = _load(SAMPLE_LABELS_FILE_PATH)
-	if result is None:
-		errorMessages.append(f"While loading sample labels: {errors}")
+	for path in (SAMPLE_LABELS_FILE_PATH, LABELS_FILE_PATH):
+		errors, result = _load(path, LABELS_SCHEMA_FILE_PATH)
+		if result is None:
+			dataType: str = "sample" if path.endswith("sample") else "user"
+			errorMessages.append(f"While loading {dataType} labels: {errors}")
+		else:
+			labels.update(result)
+	if labels:
+		return None, labels
 	else:
-		labels.update(result)
-		loaded = True
-	# Merge any new or modified labels from the user.
-	errors, result = _load(LABELS_FILE_PATH)
-	if result is None:
-		errorMessages.append(f"While loading user labels: {errors}")
-	else:
-		labels.update(result)
-		loaded = True
-	return "\n".join(errorMessages) if errorMessages else None, labels if loaded else None
+		return "\n".join(errorMessages), None
 
 
 def dumpLabels(labels: Mapping[str, str]) -> None:
-	with codecs.open(LABELS_FILE_PATH, "wb", encoding="utf-8") as fileObj:
-		json.dump(labels, fileObj, sort_keys=True, indent=2, separators=(",", ": "))
+	"""
+	Saves the labels database to disk.
+
+	Args:
+		labels: The labels database to be saved.
+	"""
+	_dump(labels, LABELS_FILE_PATH, LABELS_SCHEMA_FILE_PATH)
 
 
-def loadRooms() -> Tuple[Union[str, None], Union[Dict[str, Dict[str, Any]], None]]:
+def loadRooms() -> Union[Tuple[str, None], Tuple[None, Dict[str, Dict[str, Any]]]]:
+	"""
+	Loads the rooms database into memory.
+
+	An attempt to load the user's database is made first, otherwise the sample database is loaded.
+
+	Returns:
+		An error message or None, and the loaded rooms database or None.
+	"""
 	errorMessages: List[str] = []
-	errors, result = _load(MAP_FILE_PATH)
-	if result is None:
-		errorMessages.append(f"While loading user map: {errors}")
-	else:
-		return None, result
-	errors, result = _load(SAMPLE_MAP_FILE_PATH)
-	if result is None:
-		errorMessages.append(f"While loading sample map: {errors}")
-		return "\n".join(errorMessages), None
-	else:
-		return None, result
+	for path in (MAP_FILE_PATH, SAMPLE_MAP_FILE_PATH):
+		errors, result = _load(path, MAP_SCHEMA_FILE_PATH)
+		if result is None:
+			dataType: str = "sample" if path.endswith("sample") else "user"
+			errorMessages.append(f"While loading {dataType} map: {errors}")
+		else:
+			return None, result
+	return "\n".join(errorMessages), None
 
 
 def dumpRooms(rooms: Mapping[str, Mapping[str, Any]]) -> None:
-	with codecs.open(MAP_FILE_PATH, "wb", encoding="utf-8") as fileObj:
-		if rapidjson is not None:
-			rapidjson.dump(rooms, fileObj, sort_keys=True, indent=2, chunk_size=2 ** 16)
-		else:
-			fileObj.write(json.dumps(rooms, sort_keys=True, indent=2))
+	"""
+	Saves the rooms database to disk.
+
+	Args:
+		rooms: The rooms database to be saved.
+	"""
+	_dump(rooms, MAP_FILE_PATH, MAP_SCHEMA_FILE_PATH)
---
+++ tests/mapper/roomdata/test_database.py
@@ -0,0 +1,219 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+# Future Modules:
+from __future__ import annotations
+
+# Built-in Modules:
+from contextlib import ExitStack
+from typing import Any, Dict
+from unittest import TestCase
+from unittest.case import _AssertLogsContext
+from unittest.mock import Mock, _CallList, call, patch
+
+# Third-party Modules:
+import rapidjson
+
+# Mapper Modules:
+from mapper.roomdata.database import (
+	LABELS_FILE_PATH,
+	LABELS_SCHEMA_FILE_PATH,
+	MAP_FILE_PATH,
+	MAP_SCHEMA_FILE_PATH,
+	SAMPLE_LABELS_FILE_PATH,
+	SAMPLE_MAP_FILE_PATH,
+	SchemaValidationError,
+	_dump,
+	_load,
+	_validate,
+	dumpLabels,
+	dumpRooms,
+	loadLabels,
+	loadRooms,
+)
+
+
+ROOMS: Dict[str, Any] = {
+	"0": {
+		"align": "undefined",
+		"avoid": False,
+		"desc": "This dim tunnel is unfinished and unadorned, the walls still scarred by the picks of Dwarven",
+		"dynamicDesc": "Vig the Dwarven smelter stands here, overseeing his store.\n",
+		"exits": {
+			"east": {
+				"door": "",
+				"doorFlags": [],
+				"exitFlags": ["exit"],
+				"to": "1",
+			},
+		},
+		"light": "dark",
+		"loadFlags": [],
+		"mobFlags": ["shop"],
+		"name": "Vig's Shop",
+		"note": "",
+		"portable": "undefined",
+		"ridable": "notridable",
+		"terrain": "city",
+		"x": 22,
+		"y": 225,
+		"z": 0,
+	},
+	"1": {
+		"align": "neutral",
+		"avoid": False,
+		"desc": "The lowest level of the city begins here, where the houses and parks of the",
+		"dynamicDesc": "",
+		"exits": {
+			"west": {
+				"door": "",
+				"doorFlags": [],
+				"exitFlags": ["exit"],
+				"to": "0",
+			},
+		},
+		"light": "dark",
+		"loadFlags": [],
+		"mobFlags": [],
+		"name": "Kizdin Crafts",
+		"note": "",
+		"portable": "portable",
+		"ridable": "notridable",
+		"terrain": "city",
+		"x": 23,
+		"y": 225,
+		"z": 0,
+	},
+}
+
+
+class TestDatabase(TestCase):
+	def testValidate(self) -> None:
+		schemaPath: str = MAP_SCHEMA_FILE_PATH
+		_validate(ROOMS, schemaPath)
+		cm: ExitStack
+		with ExitStack() as cm:
+			cm.enter_context(patch("mapper.roomdata.database.jsonschema.validate"))
+			mockLogger: _AssertLogsContext = cm.enter_context(
+				self.assertLogs("mapper.roomdata.database", level="WARNING")
+			)
+			cm.enter_context(
+				self.assertRaises(SchemaValidationError, msg="rapidjson fails validation, jsonschema passes")
+			)
+			_validate({"invalid": "invalid"}, schemaPath)
+			self.assertIn(
+				"Error: jsonschema did not raise an exception, whereas rapidjson raised", mockLogger.output
+			)
+		with self.assertRaises(SchemaValidationError, msg="rapidjson and jsonschema fail validation"):
+			_validate({"invalid": "invalid"}, schemaPath)
+
+	@patch("mapper.roomdata.database._validate")
+	@patch("mapper.roomdata.database.codecs.open")
+	@patch("mapper.roomdata.database.os.path")
+	def testLoad(self, mockOSPath: Mock, mockOpen: Mock, mockValidate: Mock) -> None:
+		fileName: str = "__junk__.json"
+		schemaPath: str = MAP_SCHEMA_FILE_PATH
+		mockFileObj: Mock = Mock()
+		mockOpen.return_value.__enter__.return_value = mockFileObj
+		mockOSPath.isdir.return_value = False
+		# Check path does not exist.
+		mockOSPath.exists.return_value = False
+		errors, database = _load(fileName, schemaPath)
+		self.assertIsNotNone(errors)
+		self.assertIsNone(database)
+		mockOSPath.exists.return_value = True
+		# Check path is directory.
+		mockOSPath.isdir.return_value = True
+		errors, database = _load(fileName, schemaPath)
+		self.assertIsNotNone(errors)
+		self.assertIsNone(database)
+		mockOSPath.isdir.return_value = False
+		# Check IOError.
+		mockFileObj.read.side_effect = lambda *args: (_ for _ in ()).throw(IOError("some error"))
+		errors, database = _load(fileName, schemaPath)
+		self.assertIsNotNone(errors)
+		self.assertIsNone(database)
+		# Check corrupted data.
+		mockFileObj.read.side_effect = lambda *args: "corrupted data"
+		errors, database = _load(fileName, schemaPath)
+		self.assertIsNotNone(errors)
+		self.assertIsNone(database)
+		# Check valid data.
+		mockFileObj.read.side_effect = lambda *args: rapidjson.dumps(ROOMS)
+		errors, database = _load(fileName, schemaPath)
+		self.assertIsNone(errors)
+		self.assertIsNotNone(database)
+		mockValidate.assert_called_once_with(database, schemaPath)
+		self.assertEqual(database, ROOMS)
+
+	@patch("mapper.roomdata.database.rapidjson.dump")
+	@patch("mapper.roomdata.database._validate")
+	@patch("mapper.roomdata.database.codecs.open")
+	def testDump(self, mockOpen: Mock, mockValidate: Mock, mockJsonDump: Mock) -> None:
+		mockFileObj = Mock()
+		mockOpen.return_value.__enter__.return_value = mockFileObj
+		_dump(ROOMS, "__junk__.json", MAP_SCHEMA_FILE_PATH)
+		mockValidate.assert_called_once_with(ROOMS, MAP_SCHEMA_FILE_PATH)
+		mockJsonDump.assert_called_once_with(ROOMS, mockFileObj, sort_keys=True, indent=2, chunk_size=2 ** 16)
+
+	@patch("mapper.roomdata.database._load")
+	def testLoadLabels(self, mockLoad: Mock) -> None:
+		database: Dict[str, str] = {"label1": "1", "label2": "2"}
+		expectedErrors: str = "While loading sample labels: some_error\nWhile loading user labels: some_error"
+		mockLoad.return_value = ("some_error", None)
+		errors, labels = loadLabels()
+		self.assertEqual(errors, expectedErrors)
+		self.assertIsNone(labels)
+		loadCalls: _CallList = mockLoad.mock_calls
+		self.assertEqual(len(loadCalls), 2)
+		self.assertEqual(
+			loadCalls[0],
+			call(SAMPLE_LABELS_FILE_PATH, LABELS_SCHEMA_FILE_PATH),
+			"First call to _load was not as expected.",
+		)
+		self.assertEqual(
+			loadCalls[1],
+			call(LABELS_FILE_PATH, LABELS_SCHEMA_FILE_PATH),
+			"Second call to _load was not as expected.",
+		)
+		mockLoad.return_value = (None, database)
+		errors, labels = loadLabels()
+		self.assertIsNone(errors)
+		self.assertEqual(labels, database)
+
+	@patch("mapper.roomdata.database._dump")
+	def testDumpLabels(self, mockDump: Mock) -> None:
+		database: Dict[str, str] = {"label1": "1", "label2": "2"}
+		dumpLabels(database)
+		mockDump.assert_called_once_with(database, LABELS_FILE_PATH, LABELS_SCHEMA_FILE_PATH)
+
+	@patch("mapper.roomdata.database._load")
+	def testLoadRooms(self, mockLoad: Mock) -> None:
+		expectedErrors: str = "While loading user map: some_error\nWhile loading sample map: some_error"
+		mockLoad.return_value = ("some_error", None)
+		errors, rooms = loadRooms()
+		self.assertEqual(errors, expectedErrors)
+		self.assertIsNone(rooms)
+		loadCalls: _CallList = mockLoad.mock_calls
+		self.assertEqual(len(loadCalls), 2)
+		self.assertEqual(
+			loadCalls[0],
+			call(MAP_FILE_PATH, MAP_SCHEMA_FILE_PATH),
+			"First call to _load was not as expected.",
+		)
+		self.assertEqual(
+			loadCalls[1],
+			call(SAMPLE_MAP_FILE_PATH, MAP_SCHEMA_FILE_PATH),
+			"Second call to _load was not as expected.",
+		)
+		mockLoad.return_value = ("some_error", ROOMS)
+		errors, rooms = loadRooms()
+		self.assertIsNone(errors)
+		self.assertEqual(rooms, ROOMS)
+
+	@patch("mapper.roomdata.database._dump")
+	def testDumpRooms(self, mockDump: Mock) -> None:
+		dumpRooms(ROOMS)
+		mockDump.assert_called_once_with(ROOMS, MAP_FILE_PATH, MAP_SCHEMA_FILE_PATH)
